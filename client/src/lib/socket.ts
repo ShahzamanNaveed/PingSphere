84148eaea10bd02d7f6cbe6c6bda0f70562ae17b
Shahzaman Naveed <shahzamannaveed247@gmail.com>
2026-04-27 02:20:06 +0500

    fix: socket transports websocket only in production

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -6,6 +6,7 @@ export const connectSocket = (): Socket => {
   if (!socket) {
     socket = io(import.meta.env.VITE_API_URL, {
       withCredentials: true,
+      transports: ['websocket'],
     })
   }
   return socket
