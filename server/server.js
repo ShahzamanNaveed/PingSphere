18922ba8988ea3a74f23872d388728809d1b8aed
Shahzaman Naveed <shahzamannaveed247@gmail.com>
2026-04-27 01:09:43 +0500

    bug fix

--- server/server.js
+++ server/server.js
@@ -1,18 +1,10 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
+import 'dotenv/config'
 import http from 'http'
 import app from './app.js'
 import connectDB from './config/db.js'
 import { initSocket } from './socket/socket.js'
 
 const PORT = process.env.PORT || 5000
-
-console.log('ENV CHECK:', {
-  NODE_ENV: process.env.NODE_ENV,
-  MONGO_URI: process.env.MONGO_URI ? 'EXISTS' : 'UNDEFINED',
-  JWT_SECRET: process.env.JWT_SECRET ? 'EXISTS' : 'UNDEFINED',
-})
 
 const server = http.createServer(app)
 initSocket(server)
